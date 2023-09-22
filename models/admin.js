2c9e43d8c0457a89907c7e03054c5c94f2839895
Naveen <7naveennn@gmail.com>
2023-09-22 10:54:40 +0530

    added timestamps to all models

--- models/admin.js
+++ models/admin.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const adminSchema = new mongoose.Schema({
-  fullName: {
-    required: true,
-    type: String,
+const adminSchema = new mongoose.Schema(
+  {
+    fullName: {
+      required: true,
+      type: String,
+    },
+    email: {
+      required: true,
+      type: String,
+    },
+    password: {
+      required: true,
+      type: String,
+    },
   },
-  email: {
-    required: true,
-    type: String,
-  },
-  password: {
-    required: true,
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const Admin = mongoose.model("Admin", adminSchema);
 
